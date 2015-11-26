b0812a4f111dd7648264d210d411a4e96ee4af96
LiangCY <liangcynju@qq.com>
2015-11-26 20:35:51 +0800

    每天定时更新电影信息和票价

--- scraper/index.js
+++ scraper/index.js
@@ -6,9 +6,6 @@ var schedule = require('node-schedule');
 var Ticket = require('./ticket');
 var Movie = require('./movie');
 
-//schedule.scheduleJob('0 0 2 * * *', Movie.updateMovies);
+schedule.scheduleJob('0 0 2 * * *', Movie.updateMovies);
 
-//schedule.scheduleJob('0 0 */6 * * *', Ticket.updateTickets);
-
-//Ticket.updateTickets();
-Movie.updateMovies();
+schedule.scheduleJob('0 0 */6 * * *', Ticket.updateTickets);
